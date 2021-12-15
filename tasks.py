48f7b6115a78660861cfb35f71e30cc67c9d9171
Paul Brown <pbrown@doctorondemand.com>
2021-12-15 09:20:17 -0600

    enable rabbitmq ssl

--- tasks.py
+++ tasks.py
@@ -1,8 +1,9 @@
 import logging
+import ssl
 
 from celery import Celery
 
-app = Celery('tasks', broker='amqp://guest@rabbitmq//', broker_heartbeat=1)
+app = Celery('tasks', broker='amqp://guest@rabbitmq//', broker_use_ssl={"cert_reqs": ssl.CERT_NONE})
 
 @app.task()
 def add(x, y):
